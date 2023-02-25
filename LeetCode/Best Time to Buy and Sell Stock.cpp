4e5ea39b3a61b62bd5c6c9b578d37025faaea2d7
Najib Fikri <najibfikri13@gmail.com>
2023-02-25 20:57:58 +0700

    Best Time to Buy and Sell Stock - easy

---
+++ LeetCode/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int min_price = INT_MAX;
+        int max_profit = 0;
+        
+        for (int i = 0; i < n; i++) {
+            min_price = min(min_price, prices[i]);
+            max_profit = max(max_profit, prices[i] - min_price);
+        }
+        
+        return max_profit;
+    }
+};
