13fa87d7d245872bb96a52a5a35cc240d0d7e913
Najib Fikri <najibfikri13@gmail.com>
2024-04-21 07:39:08 +0700

    Set Matrix Zeroes - medium

---
+++ LeetCode/Set Matrix Zeroes.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstRowZero = false;
+        bool firstColZero = false;
+        
+        // Check if the first row needs to be zeroed out
+        for (int j = 0; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        
+        // Check if the first column needs to be zeroed out
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        
+        // Mark zeros on first row and first column
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        // Zero out rows and columns based on markings
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        // Zero out first row if needed
+        if (firstRowZero) {
+            for (int j = 0; j < n; ++j) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        // Zero out first column if needed
+        if (firstColZero) {
+            for (int i = 0; i < m; ++i) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    // Function to print the matrix
+    void printMatrix(vector<vector<int>>& matrix) {
+        for (auto& row : matrix) {
+            for (int num : row) {
+                cout << num << " ";
+            }
+            cout << endl;
+        }
+    }
+};
