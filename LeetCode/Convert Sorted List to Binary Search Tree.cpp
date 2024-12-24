5ab2288ae2b9a473a83ceb2e8383387c8370c4d3
Najib Fikri <najibfikri13@gmail.com>
2024-12-24 18:16:04 +0700

    Convert Sorted List to Binary Search Tree - medium

---
+++ LeetCode/Convert Sorted List to Binary Search Tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (!head) return nullptr;
+        if (!head->next) return new TreeNode(head->val);
+
+        // Use fast and slow pointers to find the middle node
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Disconnect the left half from the mid node
+        if (prev) prev->next = nullptr;
+
+        // Middle node is the root
+        TreeNode* root = new TreeNode(slow->val);
+
+        // Recursively construct the left and right subtrees
+        root->left = sortedListToBST(head); // Left half
+        root->right = sortedListToBST(slow->next); // Right half
+
+        return root;
+    }
+};
