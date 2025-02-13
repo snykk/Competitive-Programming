6e37c7ef568db99c40f9a9494b1f2eba6aedb266
Najib Fikri <najibfikri13@gmail.com>
2025-02-13 19:00:52 +0700

    Pointer - easy

---
+++ Hackerrank/C++/Pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void update(int *a, int *b) {
+    int sum = *a + *b;
+    int diff = abs(*a - *b);
+
+    *a = sum;
+    *b = diff;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    update(&a, &b);
+
+    cout << a << endl << b << endl;
+    return 0;
+}
