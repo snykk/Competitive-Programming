03005add9f6fab709ccb08f515632a8c5cc4aa23
Najib Fikri <najibfikri13@gmail.com>
2025-02-13 18:59:29 +0700

    Basic Data Types - easy

---
+++ Hackerrank/C++/Basic Data Types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+
+    scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+
+    printf("%d\n", i);
+    printf("%ld\n", l);
+    printf("%c\n", c);
+    printf("%.3f\n", f);
+    printf("%.9lf\n", d);
+
+    return 0;
+}
