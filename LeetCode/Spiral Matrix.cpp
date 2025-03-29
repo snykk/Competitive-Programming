bc5d74b9285eaba8fb30d778cd4ef1122684821e
Najib Fikri <najibfikri13@gmail.com>
2025-03-29 13:25:27 +0700

    Update "Spiral Matrix.cpp"

--- LeetCode/Spiral Matrix.cpp
+++ LeetCode/Spiral Matrix.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // using variable to allow boundaries
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> result;
         if (matrix.empty()) return result;
@@ -39,4 +40,39 @@ public:
 
         return result;
     }
+    
+    // // manipulate matrix
+    // vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    //     vector<int> result;
+
+    //     while (!matrix.empty()) {
+    //         // Add first row/list of matrix
+    //         result.insert(result.end(), matrix.front().begin(), matrix.front().end());
+    //         matrix.erase(matrix.begin());
+
+    //         // Append Last element of all list in order
+    //         if (!matrix.empty() && !matrix[0].empty()) {
+    //             for (vector<int>& row : matrix) {
+    //                 result.push_back(row.back());
+    //                 row.pop_back();
+    //             }
+    //         }
+
+    //             // Add reverse of last row/list
+    //         if (!matrix.empty()) {
+    //             result.insert(result.end(), matrix.back().rbegin(), matrix.back().rend());
+    //             matrix.pop_back();
+    //         }
+
+    //         // Append first element of all rows/list in reverse
+    //         if (!matrix.empty() && !matrix[0].empty()) {
+    //             for (int i = matrix.size() - 1; i >= 0; --i) {
+    //                 result.push_back(matrix[i].front());
+    //                 matrix[i].erase(matrix[i].begin());
+    //             }
+    //         }
+    //     }
+
+    //     return result;
+    // }
 };
