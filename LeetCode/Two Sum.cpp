2963b24c5ac015db9b819f2409fe367a7c36785b
Najib Fikri <najibfikri13@gmail.com>
2025-03-29 13:26:14 +0700

    Update "Two Sum.cpp"

--- LeetCode/Two Sum.cpp
+++ LeetCode/Two Sum.cpp
@@ -1,15 +1,33 @@
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int len = nums.size();
-        vector<int> result;
-        for (int i=0;i<len-1;i++) {
-            for (int j=i+1;j<len;j++) {
-                if (nums[i] + nums[j] == target) {
-                    result = {i,j};
+    public:
+        // greedy approach (bruteforce) -> Big(O) = O(n^2)
+        // vector<int> twoSum(vector<int>& nums, int target) {
+        //     int len = nums.size();
+        //     vector<int> result;
+        //     for (int i=0;i<len-1;i++) {
+        //         for (int j=i+1;j<len;j++) {
+        //             if (nums[i] + nums[j] == target) {
+        //                 result = {i,j};
+        //             }
+        //         }
+        //     }
+        //     return result;
+        // }
+
+        // using hashmap -> Big(O) = O(n)
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int, int> ump;
+    
+            for (int i = 0; i < nums.size(); i++) {
+                int diff = target - nums[i];
+    
+                if (ump.find(diff) != ump.end()) {
+                    return {ump[diff], i};
                 }
+    
+                ump[nums[i]] = i;
             }
+    
+            return {};
         }
-        return result;
-    }
-};
+    };
