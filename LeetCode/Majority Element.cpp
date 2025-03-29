c7557d5eae7c46b563395b44187475f13444c7f7
Najib Fikri <najibfikri13@gmail.com>
2025-03-29 13:19:09 +0700

    Majority Element - easy

---
+++ LeetCode/Majority Element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0, count = 0;
+
+        // Boyer-Moore Voting Algorithm
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+};
